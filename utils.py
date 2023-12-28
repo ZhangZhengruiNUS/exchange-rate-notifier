f4150b1122fcabb8316a6a59ae754fc5818d7c48
ZhangZhengruiNUS <e1221580@u.nus.edu>
2023-12-28 14:44:56 +0800

    1.增加MISSING_DATA_THRESHOLD参数 2.修改汇率阈值参数单词顺序为RATE_THRESHOLD 3.增加通用方法get_current_time，以便统一打印时间的格式 4.给所有print增加当前时间的前缀

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
@@ -13,14 +14,14 @@ def load_config():
         "password": os.getenv("PASSWORD"),  # 邮箱密码或专用密码
         "smtp_server": os.getenv("SMTP_SERVER"),  # SMTP服务器地址
         "smtp_port": int(os.getenv("SMTP_PORT")),  # SMTP服务器端口（SSL为465，TLS为587）
-        "threshold_rate": float(os.getenv("THRESHOLD_RATE")),  # 汇率阈值（低于此值时发送邮件）
+        "rate_threshold": float(os.getenv("RATE_THRESHOLD")),  # 汇率阈值（低于此值时发送邮件）
+        "missing_data_threshold": int(os.getenv("MISSING_DATA_THRESHOLD")),  # 连续未找到数据的阈值（高于此值时发送邮件）
         "query_interval": int(os.getenv("QUERY_INTERVAL")),  # 查询间隔，单位为秒
     }
     return config
 
 def send_email(subject, msg_body, config):
     # 创建邮件
-    print("正在创建邮件...")
     msg = MIMEMultipart()
     msg['From'] = "Notify Mate" + "<" + config["sender_email"] + ">"
     msg['To'] = config["receiver_email"]
@@ -28,11 +29,14 @@ def send_email(subject, msg_body, config):
     msg.attach(MIMEText(msg_body, 'plain'))
 
     # 发送邮件
-    print("正在发送邮件...")
+    print("[" + get_current_time() + "] 正在发送邮件...")
     server = smtplib.SMTP(config["smtp_server"], config["smtp_port"])
     server.starttls()  # 启动TLS加密
     server.login(config["sender_email"], config["password"])
     text = msg.as_string()
     server.sendmail(config["sender_email"], config["receiver_email"], text)
     server.quit()
-    print(f"已发送邮件至 {config['receiver_email']}")
+    print("[" + get_current_time() + "] 已发送邮件至 " + config['receiver_email'])
+
+def get_current_time():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
