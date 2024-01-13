5a8bfbe320f402c44df29173cc81c435afb45603
ZhangZhengruiNUS <e1221580@u.nus.edu>
2024-01-13 17:25:12 +0800

    1.将print方法替换为logging 2.在程序意外停止时，发送报警邮件

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import time
 from rate_fetcher import get_sgd_rate
 import utils
+from logger_config import setup_logger
+
+logger = setup_logger(__name__)
 
 def main():
-    print("[" + utils.get_current_time() + "] 汇率通知器开始启动...")
+    logger.info("-----汇率通知器开始启动-----")
+    unexpected_stop = True
     # 加载基础参数
     config = utils.load_config()
 
@@ -29,13 +33,22 @@ def main():
                     utils.send_email("汇率通知器报警",
                                     f"已超过设定阈值{config['missing_data_threshold']}次未找到数据，请检查服务器网络状况或网页结构是否发生变化！",
                                     config)
-                    print("[" + utils.get_current_time() + "] 汇率通知器已自动停止")
+                    logger.error(f"-----汇率通知器由于超过设定阈值{config['missing_data_threshold']}次未找到数据，已自动停止-----")
+                    unexpected_stop = False
                     break
             
             # 查询间隔
             time.sleep(config['query_interval'])
     except KeyboardInterrupt:
-        print("[" + utils.get_current_time() + "] 汇率通知器已手动停止")
+        logger.info("-----汇率通知器已手动停止-----")
+        unexpected_stop = False
+    finally:
+        # 当程序意外停止时，发送邮件
+        if unexpected_stop:
+            logger.critical("-----汇率通知器已意外停止-----")
+            utils.send_email("汇率通知器报警",
+                             "主程序由于不知名原因意外停止，请检查服务器运行状况！",
+                             config)
 
 if __name__ == '__main__':
     main()
