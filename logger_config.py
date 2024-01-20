58c238dff75cb7ea7c738671e62889c4d2a5af98
ZhangZhengruiNUS <e1221580@u.nus.edu>
2024-01-20 11:25:07 +0800

    1.将setup_logger()改为setup_global_logger()，函数内部创建logger，外部使用日志记录时，直接通过logging.getLogger获取该对象，保证程序运行时全局只有一个日志器 2.增加get_global_logger()，检查是否已经有日志配置，如果没有，进行基本配置 3.setup_global_logger()增加LOG_LEVEL配置选项

--- logger_config.py
+++ logger_config.py
@@ -8,11 +8,7 @@ from dotenv import load_dotenv
 LOG_DIR = 'logs'  # 日志文件相对目录
 LOG_FILE_PREFIX = "app"  # 日志文件前缀
 
-def setup_logger(logger_name):
-    # 如果已经创建过logger，直接返回
-    if logger_name in logging.root.manager.loggerDict:
-        return logging.getLogger(logger_name)
-    
+def setup_global_logger():
     # 创建日志文件路径
     if not os.path.exists(LOG_DIR):
         os.makedirs(LOG_DIR)
@@ -25,27 +21,31 @@ def setup_logger(logger_name):
                                             backupCount=7)  # 保留7天
     formatter = logging.Formatter('[%(asctime)s.%(msecs)03d][%(levelname)s] %(message)s', '%Y-%m-%d %H:%M:%S')  #设置日志格式
     file_handler.setFormatter(formatter)
-    file_handler.suffix = "%Y-%m-%d"
-    file_handler.namer = custom_log_file_path
 
-    # 创建logger
-    logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.INFO)
-    
-    # 添加处理器
-    logger.addHandler(file_handler)  # 添加文件日志处理器
+    # 加载环境变量
     load_dotenv()
-    if not os.getenv('ENV_TYPE', '').lower() == 'production':  # 生产环境不添加控制台日志处理器
+
+    # 设置全局日志级别
+    log_level = logging.DEBUG if os.getenv('LOG_LEVEL', '').lower() == 'debug' else logging.INFO
+    logging.basicConfig(level=log_level, handlers=[file_handler])
+
+    # 生产环境中不添加控制台日志处理器
+    if not os.getenv('ENV_TYPE', '').lower() == 'production':
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
+        logging.getLogger().addHandler(console_handler)
+        
+    logger = logging.getLogger(__name__)
+    logger.info(f"已创建全局日志记录器{__name__} 日志文件夹路径为{LOG_DIR}")
 
-    return logger
+def get_global_logger():
+    # 检查是否已经有日志配置，如果没有，进行基本配置
+    if not logging.getLogger().hasHandlers():
+        setup_global_logger()
+    return logging.getLogger(__name__)
 
-def custom_log_file_path(name=None):
+def custom_log_file_path():
     return LOG_DIR + "/" + LOG_FILE_PREFIX + "_" + datetime.now().strftime("%Y-%m-%d") + '.log'
 
 if __name__ == "__main__":
-    logger = setup_logger(__name__)
-    logger.info("这是一条普通日志")
-    logger.error("这是一条报错日志")
+    pass
