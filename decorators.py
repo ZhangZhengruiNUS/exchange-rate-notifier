23a1bd648d8a4a95dfd1c8f79c8878703bba9556
ZhangZhengruiNUS <e1221580@u.nus.edu>
2024-01-20 10:34:17 +0800

    1.改为获取全局日志对象get_global_logger() 2.timeit装饰器，只打印秒数，精确到毫秒 3.新增resources_debug_monitor()装饰器，便于打印方法执行前后资源使用情况

--- decorators.py
+++ decorators.py
@@ -1,23 +1,30 @@
 import time
 from functools import wraps
-from logger_config import setup_logger
+from utils import common_utils
+from logger_config import get_global_logger
 
-logger = setup_logger(__name__)
+# 获取全局日志对象
+logger = get_global_logger()
 
 def timeit(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        start_time = time.time()
+        start_time = time.time()  # 获取开始时间
         result = func(*args, **kwargs)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
+        end_time = time.time()  # 获取结束时间
+        exec_time = end_time - start_time  # 计算执行时间，单位为秒
 
-        # 将执行时间转换为小时、分钟和秒
-        hours, remainder = divmod(elapsed_time, 3600)
-        minutes, seconds = divmod(remainder, 60)
+        logger.info(f"{func.__name__}函数的执行时间为：{exec_time:.3f} seconds")
+        return result
+    
+    return wrapper
 
-        logger.info(f"{func.__name__}函数的执行时间为：{int(hours)}时{int(minutes)}分{int(seconds)}秒")
-
+def resources_debug_monitor(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        common_utils.log_process_resources(f"{func.__name__}函数执行前", "debug")
+        result = func(*args, **kwargs)
+        common_utils.log_process_resources(f"{func.__name__}函数执行后", "debug")
         return result
 
     return wrapper
