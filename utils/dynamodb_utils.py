5deb88947d408471ebbeb99df9e0ed3ee6a0cedb
ZhangZhengruiNUS <e1221580@u.nus.edu>
2024-01-20 10:36:59 +0800

    1.改为获取全局日志对象get_global_logger() 2.将创建 DynamoDB 资源封装为方法create_dynamodb_resource() 3.去掉内部的资源使用日志 4.对关键方法增加timeit()、resources_debug_monitor()装饰器 5.优化注释及日志打印

--- utils/dynamodb_utils.py
+++ utils/dynamodb_utils.py
@@ -1,21 +1,21 @@
-from logger_config import setup_logger
 import boto3
 from decimal import Decimal
 from datetime import datetime, timedelta
 from boto3.dynamodb.conditions import Attr
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
+from decorators import timeit, resources_debug_monitor
+from logger_config import get_global_logger
+
+# 获取全局日志对象
+logger = get_global_logger()
 
 # 常量定义
 DYNAMODB_TABLE_NAME = 'SGD_EXCHANGE_RATE'  # 日志文件相对目录
 
-logger = setup_logger(__name__)
-
+@timeit
+@resources_debug_monitor
 def write_to_dynamodb(rate):
     # 创建 DynamoDB 资源
-    logger.info("正在创建DynamoDB资源...")
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+    table = create_dynamodb_resource()
 
     # 准备数据
     data = {
@@ -29,27 +29,41 @@ def write_to_dynamodb(rate):
     table.put_item(Item=data)
     logger.info("已写入DynamoDB")
 
+@timeit
+@resources_debug_monitor
 def query_recent_days_data(day_interval=2):
-    logger.info("正在创建DynamoDB资源...")
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
-
+    # 创建 DynamoDB 资源
+    table = create_dynamodb_resource()
+    
+    # 计算起始日期
     start_date = datetime.now() - timedelta(days=(day_interval-1))
     start_date_str = start_date.strftime('%Y-%m-%d')
 
+    # 查询数据
     logger.info(f"正在查询最近{day_interval}天的数据...")
     response = table.scan(FilterExpression=Attr('Date').gte(start_date_str))
 
     if not response['Items']:
         logger.error("未找到数据！")
         return None
-
-    dates = [item['Date'] for i, item in enumerate(response['Items']) if i % 2 == 0]
-    times = [item['Time'] for i, item in enumerate(response['Items']) if i % 2 == 0]
-    rates = [float(item['Rate']) for i, item in enumerate(response['Items']) if i % 2 == 0]
-
+    logger.info(f"已查询到{len(response['Items'])}条数据...")
+    
+    # 拆分数据
+    dates = [item['Date'] for item in response['Items']]
+    times = [item['Time'] for item in response['Items']]
+    rates = [float(item['Rate']) for item in response['Items']]
+    
+    # 排序以及合并数据
     data = sorted([(datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M:%S"), rate) for date, time, rate in zip(dates, times, rates)])
+    
     return data
+
+def create_dynamodb_resource():
+    logger.info("正在创建DynamoDB资源...")
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+    logger.info("已创建DynamoDB资源")
+    return table
     
 if __name__ == '__main__':
     query_recent_days_data()
